7b181fb4d27829e3838bf7ad5082cd7b42ecc20b
Dong Liu <doliu@microsoft.com>
2018-03-29 10:16:22 +0800

    Import code

---
+++ cloud-controller-manager/main.go
@@ -0,0 +1,73 @@
+/*
+Copyright 2018 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package main
+
+import (
+	goflag "flag"
+	"fmt"
+	"math/rand"
+	"os"
+	"time"
+
+	"github.com/kubernetes/cloud-provider-azure/cloud-controller-manager/version"
+	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
+	utilflag "k8s.io/apiserver/pkg/util/flag"
+	"k8s.io/apiserver/pkg/util/logs"
+	"k8s.io/kubernetes/cmd/cloud-controller-manager/app"
+	azureprovider "k8s.io/kubernetes/pkg/cloudprovider/providers/azure"
+)
+
+func main() {
+	rand.Seed(time.Now().UTC().UnixNano())
+
+	command := app.NewCloudControllerManagerCommand()
+	pflag.CommandLine.SetNormalizeFunc(utilflag.WordSepNormalizeFunc)
+	pflag.CommandLine.AddGoFlagSet(goflag.CommandLine)
+
+	logs.InitLogs()
+	defer logs.FlushLogs()
+
+	// setup for azure
+	var versionFlag *pflag.Value
+	command.Flags().VisitAll(func(flag *pflag.Flag) {
+		if flag.Name == "cloud-provider" {
+			flag.Value.Set(azureprovider.CloudProviderName)
+			flag.DefValue = azureprovider.CloudProviderName
+		}
+	})
+
+	pflag.CommandLine.VisitAll(func(flag *pflag.Flag) {
+		if flag.Name == "version" {
+			versionFlag = &flag.Value
+		}
+	})
+
+	command.Use = version.ApplicationName
+	innerRun := command.Run
+	command.Run = func(cmd *cobra.Command, args []string) {
+		if versionFlag != nil && (*versionFlag).String() != "false" {
+			version.PrintAndExit()
+		}
+		innerRun(cmd, args)
+	}
+
+	if err := command.Execute(); err != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", err)
+		os.Exit(1)
+	}
+}
