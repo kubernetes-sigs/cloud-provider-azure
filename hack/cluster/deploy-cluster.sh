6f7e2c7fd9ae50ae082e4cf8be08e7bdc4251b98
Abhik Chakraborty <31558638+Abhik1998@users.noreply.github.com>
2019-04-03 15:12:40 +0530

    Update deploy-cluster.sh

--- hack/cluster/deploy-cluster.sh
+++ hack/cluster/deploy-cluster.sh
@@ -12,7 +12,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-az login
 echo "Put name of resource to be created"
 read resrc
 echo "Enter location"
@@ -23,18 +22,26 @@ read id2
 echo "Type the filename of the generated api-model to be used in .json format"
 read filename
 echo "Please edit dnsPrefix,keydata,clientId and secret"
-gedit $filename
+gedit ../tests/k8s-azure/manifest/$filename
 az ad sp create-for-rbac --role="Contributor" --scopes="/subscriptions/$id2/resourceGroups/$resrc"
 echo "Paste the  name"
 read cliedId
 echo "Paste the password here"
 read clientscr
-aks-engine deploy --subscription-id $id2 \
- --auth-method cli \
- --dns-prefix $resrc \
- --resource-group $resrc \
- --location $location \
- --api-model $filename \
- --set servicePrincipalProfile.clientId="$clientId" \
- --set servicePrincipalProfile.secret="$clientscr"
-# deployed cluster successfully.
+echo "Do you have aks-engine installed?[Yes]/[No]"
+read e
+if [ $e = 'Yes' ]
+then
+echo "Let's go ahead"
+  aks-engine deploy --subscription-id $id2 \
+  --auth-method cli \
+  --dns-prefix $resrc \
+  --resource-group $resrc \
+  --location $location \
+  --api-model ../tests/k8s-azure/manifest/$filename \
+  --set servicePrincipalProfile.clientId="$clientId" \
+  --set servicePrincipalProfile.secret="$clientscr"
+ #deployed cluster successfully
+else
+echo "Please refer to link https://github.com/Azure/aks-engine/blob/master/docs/tutorials/quickstart.md"
+fi
