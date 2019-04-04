77bf7f5916ec8ca745d378402e90c5313776d5c9
Abhik Chakraborty <31558638+Abhik1998@users.noreply.github.com>
2019-04-04 14:39:29 +0530

    Update deploy-cluster.sh

--- hack/deploy-cluster.sh
+++ hack/deploy-cluster.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright 2018 The Kubernetes Authors.
+# Copyright 2019 The Kubernetes Authors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,36 +12,70 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-echo "Put name of resource to be created"
-read resrc
-echo "Enter location"
-read location
-az group create --name $resrc --location $location
-echo "Paste your subscription id here"
-read id2
-echo "Type the filename of the generated api-model to be used in .json format"
-read filename
-echo "Please edit dnsPrefix,keydata,clientId and secret"
-gedit ../tests/k8s-azure/manifest/$filename
-az ad sp create-for-rbac --role="Contributor" --scopes="/subscriptions/$id2/resourceGroups/$resrc"
-echo "Paste the  name"
-read cliedId
-echo "Paste the password here"
-read clientscr
-echo "Do you have aks-engine installed?[Yes]/[No]"
-read e
-if [ $e = 'Yes' ]
+set -e
+
+RESOURCE_GROUP_NAME=${RESOURCE_GROUP_NAME:-""}
+LOCATION=${LOCATION:-""}
+
+#check for variables is initialized or not
+
+if [ "$RESOURCE_GROUP_NAME" = "" ]
 then
-echo "Let's go ahead"
-  aks-engine deploy --subscription-id $id2 \
+echo "RESOURCE GROUP NAME must be specified"
+exit 1
+fi
+if [ "$LOCATION" = "" ]
+then
+echo "LOCATION must be specified"
+exit 1
+fi
+
+#getting the subscription id
+
+INPUT=$(az group create --name $RESOURCE_GROUP_NAME --location $LOCATION)
+CUT_ID=$(echo $INPUT| cut -d"/" -f 3)
+SUBSCRIPTION_ID=${SUBSCRIPTION_ID:-"$CUT_ID"}
+
+#getting client_id
+
+INPUT1=$(az ad sp create-for-rbac --role="Contributor" --scopes="/subscriptions/$SUBSCRIPTION_ID/resourceGroups/$resrc")
+SUB1=$(echo $INPUT1| cut -d":" -f 2)
+SUB2=$(echo $SUB1| cut -d"," -f 1)
+CLIENT_ID=${CLIENT_ID:-"$SUB2"}
+
+#getting client_secret
+
+SUB3=$(echo $INPUT1| cut -d":" -f 2)
+SUB4=$(echo $SUB3| cut -d"," -f 1)
+CLIENT_SECRET=${CLIENT_SECRET:-"$SUB4"}
+
+#getting tenant_id
+
+SUB5=$(echo $INPUT1| cut -d":" -f 7-)
+SUB6=$(echo $SUB5| cut -d"}" -f 1)
+TENANT_ID=${TENANT_ID:-"$SUB6"}
+
+
+echo "Type the filename of the generated api-model to be used in .json format"
+read FILENAME
+echo "Client_id is $CLIENT_ID"
+echo "Client_secret is $CLIENT_SECRET"
+echo "Please edit dnsPrefix,keydata,clientId and secret"
+gedit ../tests/k8s-azure/manifest/$FILENAME
+
+check=$(aks-engine version)
+if [ "$check" != "" ]
+then
+echo "Ok! aks-engine installed.Let go ahead"
+  aks-engine deploy --subscription-id $SUBSCRIPTION_ID \
   --auth-method cli \
-  --dns-prefix $resrc \
-  --resource-group $resrc \
-  --location $location \
-  --api-model ../tests/k8s-azure/manifest/$filename \
-  --set servicePrincipalProfile.clientId="$clientId" \
-  --set servicePrincipalProfile.secret="$clientscr"
+  --dns-prefix \
+  --resource-group $RESOURCE_GROUP_NAME \
+  --location $LOCATION \
+  --api-model ../tests/k8s-azure/manifest/$FILENAME \
+  --set servicePrincipalProfile.clientId="$CLIENT_ID" \
+  --set servicePrincipalProfile.secret="$CLIENT_SECRET"
  #deployed cluster successfully
 else
-echo "Please refer to link https://github.com/Azure/aks-engine/blob/master/docs/tutorials/quickstart.md"
+echo "aks-engine not installed.Please refer to link https://github.com/Azure/aks-engine/blob/master/docs/tutorials/quickstart.md"
 fi
