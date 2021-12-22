fe77ecbbe4aa1d1e5da974e55f069f743235e2f1
Shangxiang Fan <shafan@microsoft.com>
2021-12-22 11:44:39 +0000

    add numOfProbe and probeInterval in health probe configuration
    Signed-off-by: Shangxiang Fan <shafan@microsoft.com>
    Signed-off-by: MartinForReal <fanshangxiang@gmail.com>

--- pkg/provider/azure_loadbalancer_test.go
+++ pkg/provider/azure_loadbalancer_test.go
@@ -33,10 +33,8 @@ import (
 	"github.com/Azure/go-autorest/autorest/to"
 	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/assert"
-
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-
 	"sigs.k8s.io/cloud-provider-azure/pkg/azureclients/loadbalancerclient/mockloadbalancerclient"
 	"sigs.k8s.io/cloud-provider-azure/pkg/azureclients/publicipclient/mockpublicipclient"
 	"sigs.k8s.io/cloud-provider-azure/pkg/azureclients/securitygroupclient/mocksecuritygroupclient"
