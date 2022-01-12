5b8e404d5906e93068aea10db63cc2de18890267
andyzhangx <xiazhang@microsoft.com>
2022-01-12 09:27:34 +0000

    upgrade to compute sdk 2020-12-01
    fix test

--- tests/e2e/utils/vmss_utils.go
+++ tests/e2e/utils/vmss_utils.go
@@ -22,7 +22,7 @@ import (
 	"regexp"
 	"strings"
 
-	azcompute "github.com/Azure/azure-sdk-for-go/services/compute/mgmt/2020-12-01/compute"
+	azcompute "github.com/Azure/azure-sdk-for-go/services/compute/mgmt/2021-07-01/compute"
 	"github.com/Azure/go-autorest/autorest/to"
 
 	"k8s.io/apimachinery/pkg/util/wait"
@@ -59,7 +59,7 @@ func ScaleVMSS(tc *AzureTestClient, vmssName, rgName string, instanceCount int64
 
 	vmssClient := tc.createVMSSClient()
 
-	vmss, err := vmssClient.Get(context.Background(), rgName, vmssName)
+	vmss, err := vmssClient.Get(context.Background(), rgName, vmssName, azcompute.ExpandTypesForGetVMScaleSetsUserData)
 	if err != nil {
 		return err
 	}
