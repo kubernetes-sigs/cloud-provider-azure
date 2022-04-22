ace9230fe62299a67610b8a75358bb017edaa95f
lodrem <jialun.cai@pm.me>
2022-04-22 10:01:27 +0800

    test: add e2e test for deletion of VMSS instance

--- tests/e2e/network/node.go
+++ tests/e2e/network/node.go
@@ -104,12 +104,12 @@ var _ = Describe("Azure node resources", func() {
 		vmsses, err := utils.ListVMSSes(tc)
 		Expect(utils.HandleVMSSNotFoundErr(err)).To(BeTrue())
 
-		if vmsses != nil && len(*vmsses) != 0 {
-			for _, vmss := range *vmsses {
+		if len(vmsses) != 0 {
+			for _, vmss := range vmsses {
 				vmssVMs, err := utils.ListVMSSVMs(tc, *vmss.Name)
 				Expect(err).NotTo(HaveOccurred())
 
-				for _, vmssVM := range *vmssVMs {
+				for _, vmssVM := range vmssVMs {
 					nodeName, err := utils.GetVMSSVMComputerName(vmssVM)
 					Expect(err).NotTo(HaveOccurred())
 
@@ -181,11 +181,11 @@ var _ = Describe("Azure node resources", func() {
 		utils.Logf("getting all NICs of VMSSes")
 		var vmssAllNics []network.Interface
 		vmssVMs := make([]compute.VirtualMachineScaleSetVM, 0)
-		if vmsses != nil && len(*vmsses) != 0 {
-			for _, vmss := range *vmsses {
+		if len(vmsses) != 0 {
+			for _, vmss := range vmsses {
 				vmssVMList, err := utils.ListVMSSVMs(tc, *vmss.Name)
 				Expect(err).NotTo(HaveOccurred())
-				vmssVMs = append(vmssVMs, *vmssVMList...)
+				vmssVMs = append(vmssVMs, vmssVMList...)
 
 				vmssNics, err := utils.ListVMSSNICs(tc, *vmss.Name)
 				Expect(err).NotTo(HaveOccurred())
