3aa2e269ff4b2d7b18eb8b4d10202c1298f1d05f
Zhecheng Li <zhechengli@microsoft.com>
2022-05-25 17:45:43 +0800

    [e2e][Windows] Adjust Service validation for Windows cluster * Use agnhost image to support Windows Service validation.   nginx image is only for Linux. Allocate it to control plane node * Customize deployment's port configuration to support   more testcases (tcp + udp) * Replace curl with nc to test UDP traffic * Unify validate connectivity methods for external & internal IP * Bug fix: validate connectivity should also consider   non-80 port, instead of skipping * Use HostExecPod's IP as LoadBalancerSourceRanges to   validate rule * Use 2 deployments & services for shared pls test * refactor
    Signed-off-by: Zhecheng Li <zhechengli@microsoft.com>

--- tests/e2e/node/vmss.go
+++ tests/e2e/node/vmss.go
@@ -93,7 +93,7 @@ var _ = Describe("Lifecycle of VMSS", Label(utils.TestSuiteLabelVMSS), func() {
 
 			vmssAfterTest, err := utils.GetVMSS(azCli, *vmss.Name)
 			Expect(err).NotTo(HaveOccurred())
-			utils.Logf("VMSS %q sku capacity after the test: %d", &vmssAfterTest.Name, *vmssAfterTest.Sku.Capacity)
+			utils.Logf("VMSS %q sku capacity after the test: %d", *vmssAfterTest.Name, *vmssAfterTest.Sku.Capacity)
 		}()
 
 		err = utils.ValidateClusterNodesMatchVMSSInstances(azCli, expectedCap)
@@ -135,7 +135,7 @@ var _ = Describe("Lifecycle of VMSS", Label(utils.TestSuiteLabelVMSS), func() {
 
 			vmssAfterTest, err := utils.GetVMSS(azCli, *vmss.Name)
 			Expect(err).NotTo(HaveOccurred())
-			utils.Logf("VMSS %q sku capacity after the test: %d", &vmssAfterTest.Name, *vmssAfterTest.Sku.Capacity)
+			utils.Logf("VMSS %q sku capacity after the test: %d", *vmssAfterTest.Name, *vmssAfterTest.Sku.Capacity)
 		}()
 
 		err = utils.ValidateClusterNodesMatchVMSSInstances(azCli, expectedCap)
