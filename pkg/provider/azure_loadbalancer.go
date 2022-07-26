2fa9f14da1cee4ad250e8363fe1d8c2a7e431c3c
Zhecheng Li <zhechengli@microsoft.com>
2022-07-26 15:32:49 +0800

    Skip "instance not found" error for LB backend address pools
    Signed-off-by: Zhecheng Li <zhechengli@microsoft.com>

--- pkg/provider/azure_loadbalancer.go
+++ pkg/provider/azure_loadbalancer.go
@@ -98,7 +98,9 @@ func (az *Cloud) reconcileService(ctx context.Context, clusterName string, servi
 	lbStatus, fipConfig, err := az.getServiceLoadBalancerStatus(service, lb, nil)
 	if err != nil {
 		klog.Errorf("getServiceLoadBalancerStatus(%s) failed: %v", serviceName, err)
-		return nil, err
+		if !errors.Is(err, ErrorNotVmssInstance) {
+			return nil, err
+		}
 	}
 
 	var serviceIP *string
