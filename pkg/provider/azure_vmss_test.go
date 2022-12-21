afc4cb3f01b68a4419e7540bfde511157f7a08c5
andyzhangx <xiazhang@microsoft.com>
2022-12-21 13:21:12 +0000

    fix: race condition in lockMap

--- pkg/provider/azure_vmss_test.go
+++ pkg/provider/azure_vmss_test.go
@@ -2598,6 +2598,13 @@ func TestEnsureBackendPoolDeleted(t *testing.T) {
 	}
 }
 
+func TestEnsureBackendPoolDeletedConcurrentlyLoop(t *testing.T) {
+	// run TestEnsureBackendPoolDeletedConcurrently 20 times to detect race conditions
+	for i := 0; i < 20; i++ {
+		TestEnsureBackendPoolDeletedConcurrently(t)
+	}
+}
+
 func TestEnsureBackendPoolDeletedConcurrently(t *testing.T) {
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
