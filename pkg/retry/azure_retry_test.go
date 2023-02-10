0c7d8e09a3e38fdc194a744d17f78a32cbf8c0e7
Zhecheng Li <zhechengli@microsoft.com>
2023-02-10 08:27:28 +0000

    Fix TestJitterWithNegativeMaxFactor UT Gap between expected and actual value can be negative but its absolute value is still < 1.
    Signed-off-by: Zhecheng Li <zhechengli@microsoft.com>

--- pkg/retry/azure_retry_test.go
+++ pkg/retry/azure_retry_test.go
@@ -82,8 +82,8 @@ func TestJitterWithNegativeMaxFactor(t *testing.T) {
 	res := jitter(duration, maxFactor)
 	defaultMaxFactor := 1.0
 	expected := jitter(duration, defaultMaxFactor)
-	assert.Equal(t, expected-res >= time.Duration(0.0*float64(duration)), true)
-	assert.Equal(t, expected-res < time.Duration(1.0*float64(duration)), true)
+	gap := expected - res
+	assert.Lessf(t, gap.Abs(), time.Duration(1.0*float64(duration)), "expected %v, result %v", expected, res)
 }
 
 func TestDoExponentialBackoffRetry(t *testing.T) {
