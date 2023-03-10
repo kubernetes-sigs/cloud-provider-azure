546d51740b2d0d99750297e8157d12009a4bdc5f
Zhecheng Li <zhechengli@microsoft.com>
2023-03-10 01:32:42 +0000

    Enable azure-load-balancer-resource-group test Before, there's an issue with upstream pipeline on ARM so it is only tested in AKS pipeline. Now, it seems recovered.
    Signed-off-by: Zhecheng Li <zhechengli@microsoft.com>

--- tests/e2e/network/service_annotations.go
+++ tests/e2e/network/service_annotations.go
@@ -327,9 +327,6 @@ var _ = Describe("Service with annotation", Label(utils.TestSuiteLabelServiceAnn
 	})
 
 	It("should support service annotation 'service.beta.kubernetes.io/azure-load-balancer-resource-group'", func() {
-		if os.Getenv(utils.AKSTestCCM) != utils.TrueValue {
-			Skip("Skip this test because ARM is not working properly on testgrid testbeds")
-		}
 		By("creating a test resource group")
 		rg, cleanup := utils.CreateTestResourceGroup(tc)
 		defer cleanup(pointer.StringDeref(rg.Name, ""))
