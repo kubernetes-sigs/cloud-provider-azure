5017c96aafe3c6a4edb4063ff8189e2558a8bb11
MartinForReal <fanshangxiang@gmail.com>
2023-03-23 04:10:02 +0000

    deprecate numberOfProbes and adopt ProbeThreshold to address a probe issue in nrp

--- tests/e2e/network/service_annotations.go
+++ tests/e2e/network/service_annotations.go
@@ -677,8 +677,8 @@ var _ = Describe("Service with annotation", Label(utils.TestSuiteLabelServiceAnn
 		var numberOfProbes *int32
 		var intervalInSeconds *int32
 		for _, probe := range targetProbes {
-			if probe.NumberOfProbes != nil {
-				numberOfProbes = probe.NumberOfProbes
+			if probe.ProbeThreshold != nil {
+				numberOfProbes = probe.ProbeThreshold
 			}
 			if probe.IntervalInSeconds != nil {
 				intervalInSeconds = probe.IntervalInSeconds
@@ -741,8 +741,8 @@ var _ = Describe("Service with annotation", Label(utils.TestSuiteLabelServiceAnn
 		var numberOfProbes *int32
 		var intervalInSeconds *int32
 		for _, probe := range targetProbes {
-			if probe.NumberOfProbes != nil {
-				numberOfProbes = probe.NumberOfProbes
+			if probe.ProbeThreshold != nil {
+				numberOfProbes = probe.ProbeThreshold
 			}
 			if probe.IntervalInSeconds != nil {
 				intervalInSeconds = probe.IntervalInSeconds
