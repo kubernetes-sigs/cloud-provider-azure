dba7928878a161b34c5e74cac2ae1dd482fb0aea
Joel Speed <joel.speed@hotmail.co.uk>
2023-04-03 11:14:18 +0100

    Allow deprecated beta topology labels to be applied for those not ready to migrate
    This reverts commit 67c240792c4217bddaef147f1a1c5761afe006d3.
    It then introduces a new flag to allow users to opt-in to syncing older, beta
    topology labels based on the presence of the GA topolgoy labels.

--- pkg/nodemanager/nodemanager_test.go
+++ pkg/nodemanager/nodemanager_test.go
@@ -19,7 +19,7 @@ package nodemanager
 import (
 	"context"
 	"errors"
-	"fmt"
+	"reflect"
 	"testing"
 	"time"
 
@@ -190,6 +190,7 @@ func TestNodeInitialized(t *testing.T) {
 		fnh,
 		mockNP,
 		time.Second,
+		false,
 		false)
 
 	cloudNodeController.AddCloudNode(ctx, fnh.Existing[0])
@@ -268,7 +269,8 @@ func TestUpdateCloudNode(t *testing.T) {
 		fnh,
 		mockNP,
 		time.Second,
-		true)
+		true,
+		false)
 	eventBroadcaster.StartLogging(klog.Infof)
 
 	cloudNodeController.UpdateCloudNode(ctx, fnh.Existing[0], fnh.Existing[0])
@@ -319,6 +321,7 @@ func TestNodeIgnored(t *testing.T) {
 		fnh,
 		mockNP,
 		time.Second,
+		false,
 		false)
 	eventBroadcaster.StartLogging(klog.Infof)
 
@@ -333,83 +336,172 @@ func TestZoneInitialized(t *testing.T) {
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
 
-	fnh := &testutil.FakeNodeHandler{
-		Existing: []*v1.Node{
-			{
-				ObjectMeta: metav1.ObjectMeta{
-					Name:              "node0",
-					CreationTimestamp: metav1.Date(2012, 1, 1, 0, 0, 0, 0, time.UTC),
-					Labels:            map[string]string{},
-				},
-				Status: v1.NodeStatus{
-					Conditions: []v1.NodeCondition{
-						{
-							Type:               v1.NodeReady,
-							Status:             v1.ConditionUnknown,
-							LastHeartbeatTime:  metav1.Date(2015, 1, 1, 12, 0, 0, 0, time.UTC),
-							LastTransitionTime: metav1.Date(2015, 1, 1, 12, 0, 0, 0, time.UTC),
+	t.Run("with stable zone labels", func(t *testing.T) {
+		fnh := &testutil.FakeNodeHandler{
+			Existing: []*v1.Node{
+				{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:              "node0",
+						CreationTimestamp: metav1.Date(2012, 1, 1, 0, 0, 0, 0, time.UTC),
+						Labels:            map[string]string{},
+					},
+					Status: v1.NodeStatus{
+						Conditions: []v1.NodeCondition{
+							{
+								Type:               v1.NodeReady,
+								Status:             v1.ConditionUnknown,
+								LastHeartbeatTime:  metav1.Date(2015, 1, 1, 12, 0, 0, 0, time.UTC),
+								LastTransitionTime: metav1.Date(2015, 1, 1, 12, 0, 0, 0, time.UTC),
+							},
 						},
 					},
-				},
-				Spec: v1.NodeSpec{
-					Taints: []v1.Taint{
-						{
-							Key:    cloudproviderapi.TaintExternalCloudProvider,
-							Value:  "true",
-							Effect: v1.TaintEffectNoSchedule,
+					Spec: v1.NodeSpec{
+						Taints: []v1.Taint{
+							{
+								Key:    cloudproviderapi.TaintExternalCloudProvider,
+								Value:  "true",
+								Effect: v1.TaintEffectNoSchedule,
+							},
 						},
 					},
 				},
 			},
-		},
-		Clientset:      fake.NewSimpleClientset(&v1.PodList{}),
-		DeleteWaitChan: make(chan struct{}),
-	}
+			Clientset:      fake.NewSimpleClientset(&v1.PodList{}),
+			DeleteWaitChan: make(chan struct{}),
+		}
 
-	ctx := context.TODO()
-	factory := informers.NewSharedInformerFactory(fnh, 0)
-	mockNP := mocknodeprovider.NewMockNodeProvider(ctrl)
-	mockNP.EXPECT().InstanceID(ctx, types.NodeName("node0")).Return("node0", nil)
-	mockNP.EXPECT().InstanceType(ctx, types.NodeName("node0")).Return("Standard_D2_v3", nil)
-	mockNP.EXPECT().GetZone(ctx, gomock.Any()).Return(cloudprovider.Zone{
-		Region:        "eastus",
-		FailureDomain: "eastus-1",
-	}, nil)
-	mockNP.EXPECT().NodeAddresses(ctx, types.NodeName("node0")).Return([]v1.NodeAddress{
-		{
-			Type:    v1.NodeHostName,
-			Address: "node0.cloud.internal",
-		},
-		{
-			Type:    v1.NodeInternalIP,
-			Address: "10.0.0.1",
-		},
-		{
-			Type:    v1.NodeExternalIP,
-			Address: "132.143.154.163",
-		},
-	}, nil).AnyTimes()
-	mockNP.EXPECT().GetPlatformSubFaultDomain().Return("", nil)
+		ctx := context.TODO()
+		factory := informers.NewSharedInformerFactory(fnh, 0)
+		mockNP := mocknodeprovider.NewMockNodeProvider(ctrl)
+		mockNP.EXPECT().InstanceID(ctx, types.NodeName("node0")).Return("node0", nil)
+		mockNP.EXPECT().InstanceType(ctx, types.NodeName("node0")).Return("Standard_D2_v3", nil)
+		mockNP.EXPECT().GetZone(ctx, gomock.Any()).Return(cloudprovider.Zone{
+			Region:        "eastus",
+			FailureDomain: "eastus-1",
+		}, nil)
+		mockNP.EXPECT().NodeAddresses(ctx, types.NodeName("node0")).Return([]v1.NodeAddress{
+			{
+				Type:    v1.NodeHostName,
+				Address: "node0.cloud.internal",
+			},
+			{
+				Type:    v1.NodeInternalIP,
+				Address: "10.0.0.1",
+			},
+			{
+				Type:    v1.NodeExternalIP,
+				Address: "132.143.154.163",
+			},
+		}, nil).AnyTimes()
+		mockNP.EXPECT().GetPlatformSubFaultDomain().Return("", nil)
 
-	eventBroadcaster := record.NewBroadcaster()
-	cloudNodeController := &CloudNodeController{
-		kubeClient:   fnh,
-		nodeName:     "node0",
-		nodeProvider: mockNP,
-		nodeInformer: factory.Core().V1().Nodes(),
-		recorder:     eventBroadcaster.NewRecorder(scheme.Scheme, v1.EventSource{Component: "cloud-node-controller"}),
-	}
-	eventBroadcaster.StartLogging(klog.Infof)
+		eventBroadcaster := record.NewBroadcaster()
+		cloudNodeController := &CloudNodeController{
+			kubeClient:   fnh,
+			nodeName:     "node0",
+			nodeProvider: mockNP,
+			nodeInformer: factory.Core().V1().Nodes(),
+			recorder:     eventBroadcaster.NewRecorder(scheme.Scheme, v1.EventSource{Component: "cloud-node-controller"}),
+		}
+		eventBroadcaster.StartLogging(klog.Infof)
 
-	cloudNodeController.AddCloudNode(context.TODO(), fnh.Existing[0])
+		cloudNodeController.AddCloudNode(context.TODO(), fnh.Existing[0])
 
-	assert.Equal(t, 1, len(fnh.UpdatedNodes), "Node was not updated")
-	fmt.Println(fnh.UpdatedNodes[0].ObjectMeta.Labels)
-	assert.Equal(t, "eastus", fnh.UpdatedNodes[0].ObjectMeta.Labels[v1.LabelZoneRegionStable])
-	assert.Equal(t, "eastus-1", fnh.UpdatedNodes[0].ObjectMeta.Labels[v1.LabelZoneFailureDomainStable])
-	assert.Equal(t, "node0", fnh.UpdatedNodes[0].Name, "Node was not updated")
-	assert.Equal(t, 3, len(fnh.UpdatedNodes[0].ObjectMeta.Labels),
-		"Node label for Region and Zone were not set")
+		assert.Equal(t, 1, len(fnh.UpdatedNodes), "Node was not updated")
+		assert.Equal(t, "node0", fnh.UpdatedNodes[0].Name, "Node was not updated")
+		assert.Equal(t, 3, len(fnh.UpdatedNodes[0].ObjectMeta.Labels),
+			"Node label for Region and Zone were not set")
+		assert.Equal(t, "eastus", fnh.UpdatedNodes[0].ObjectMeta.Labels[v1.LabelZoneRegionStable],
+			"Node Region not correctly updated")
+		assert.Equal(t, "eastus-1", fnh.UpdatedNodes[0].ObjectMeta.Labels[v1.LabelZoneFailureDomainStable],
+			"Node FailureDomain not correctly updated")
+	})
+
+	t.Run("with beta zone labels", func(t *testing.T) {
+		fnh := &testutil.FakeNodeHandler{
+			Existing: []*v1.Node{
+				{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:              "node0",
+						CreationTimestamp: metav1.Date(2012, 1, 1, 0, 0, 0, 0, time.UTC),
+						Labels:            map[string]string{},
+					},
+					Status: v1.NodeStatus{
+						Conditions: []v1.NodeCondition{
+							{
+								Type:               v1.NodeReady,
+								Status:             v1.ConditionUnknown,
+								LastHeartbeatTime:  metav1.Date(2015, 1, 1, 12, 0, 0, 0, time.UTC),
+								LastTransitionTime: metav1.Date(2015, 1, 1, 12, 0, 0, 0, time.UTC),
+							},
+						},
+					},
+					Spec: v1.NodeSpec{
+						Taints: []v1.Taint{
+							{
+								Key:    cloudproviderapi.TaintExternalCloudProvider,
+								Value:  "true",
+								Effect: v1.TaintEffectNoSchedule,
+							},
+						},
+					},
+				},
+			},
+			Clientset:      fake.NewSimpleClientset(&v1.PodList{}),
+			DeleteWaitChan: make(chan struct{}),
+		}
+
+		ctx := context.TODO()
+		factory := informers.NewSharedInformerFactory(fnh, 0)
+		mockNP := mocknodeprovider.NewMockNodeProvider(ctrl)
+		mockNP.EXPECT().InstanceID(ctx, types.NodeName("node0")).Return("node0", nil)
+		mockNP.EXPECT().InstanceType(ctx, types.NodeName("node0")).Return("Standard_D2_v3", nil)
+		mockNP.EXPECT().GetZone(ctx, gomock.Any()).Return(cloudprovider.Zone{
+			Region:        "eastus",
+			FailureDomain: "eastus-1",
+		}, nil)
+		mockNP.EXPECT().NodeAddresses(ctx, types.NodeName("node0")).Return([]v1.NodeAddress{
+			{
+				Type:    v1.NodeHostName,
+				Address: "node0.cloud.internal",
+			},
+			{
+				Type:    v1.NodeInternalIP,
+				Address: "10.0.0.1",
+			},
+			{
+				Type:    v1.NodeExternalIP,
+				Address: "132.143.154.163",
+			},
+		}, nil).AnyTimes()
+		mockNP.EXPECT().GetPlatformSubFaultDomain().Return("", nil)
+
+		eventBroadcaster := record.NewBroadcaster()
+		cloudNodeController := &CloudNodeController{
+			kubeClient:               fnh,
+			nodeName:                 "node0",
+			nodeProvider:             mockNP,
+			nodeInformer:             factory.Core().V1().Nodes(),
+			recorder:                 eventBroadcaster.NewRecorder(scheme.Scheme, v1.EventSource{Component: "cloud-node-controller"}),
+			enableBetaTopologyLabels: true,
+		}
+		eventBroadcaster.StartLogging(klog.Infof)
+
+		cloudNodeController.AddCloudNode(context.TODO(), fnh.Existing[0])
+
+		assert.Equal(t, 1, len(fnh.UpdatedNodes), "Node was not updated")
+		assert.Equal(t, "node0", fnh.UpdatedNodes[0].Name, "Node was not updated")
+		assert.Equal(t, 6, len(fnh.UpdatedNodes[0].ObjectMeta.Labels),
+			"Node label for Region and Zone were not set")
+		assert.Equal(t, "eastus", fnh.UpdatedNodes[0].ObjectMeta.Labels[v1.LabelZoneRegionStable],
+			"Node Region not correctly updated")
+		assert.Equal(t, "eastus-1", fnh.UpdatedNodes[0].ObjectMeta.Labels[v1.LabelZoneFailureDomainStable],
+			"Node FailureDomain not correctly updated")
+		assert.Equal(t, "eastus", fnh.UpdatedNodes[0].ObjectMeta.Labels[v1.LabelZoneRegion],
+			"Node Region not correctly updated")
+		assert.Equal(t, "eastus-1", fnh.UpdatedNodes[0].ObjectMeta.Labels[v1.LabelZoneFailureDomain],
+			"Node FailureDomain not correctly updated")
+	})
 }
 
 // This test checks that a node with the external cloud provider taint is cloudprovider initialized and
@@ -461,7 +553,10 @@ func TestAddCloudNode(t *testing.T) {
 	mockNP := mocknodeprovider.NewMockNodeProvider(ctrl)
 	mockNP.EXPECT().InstanceID(gomock.Any(), types.NodeName("node0")).Return("node0", nil)
 	mockNP.EXPECT().InstanceType(gomock.Any(), types.NodeName("node0")).Return("Standard_D2_v3", nil)
-
+	mockNP.EXPECT().GetZone(gomock.Any(), gomock.Any()).Return(cloudprovider.Zone{
+		Region:        "eastus",
+		FailureDomain: "eastus-1",
+	}, nil)
 	mockNP.EXPECT().NodeAddresses(gomock.Any(), types.NodeName("node0")).Return([]v1.NodeAddress{
 		{
 			Type:    v1.NodeHostName,
@@ -476,10 +571,6 @@ func TestAddCloudNode(t *testing.T) {
 			Address: "132.143.154.163",
 		},
 	}, nil).AnyTimes()
-	mockNP.EXPECT().GetZone(gomock.Any(), gomock.Any()).Return(cloudprovider.Zone{
-		Region:        "eastus",
-		FailureDomain: "eastus-1",
-	}, nil)
 	mockNP.EXPECT().GetPlatformSubFaultDomain().Return("", nil)
 
 	factory := informers.NewSharedInformerFactory(fnh, 0)
@@ -491,6 +582,7 @@ func TestAddCloudNode(t *testing.T) {
 		fnh,
 		mockNP,
 		time.Second,
+		false,
 		false)
 	factory.Start(ctx.Done())
 	cache.WaitForCacheSync(ctx.Done(), nodeInformer.Informer().HasSynced)
@@ -550,6 +642,7 @@ func TestUpdateNodeAddresses(t *testing.T) {
 		fnh,
 		mockNP,
 		time.Second,
+		false,
 		false)
 	factory.Start(ctx.Done())
 	cache.WaitForCacheSync(ctx.Done(), nodeInformer.Informer().HasSynced)
@@ -651,6 +744,7 @@ func TestNodeProvidedIPAddresses(t *testing.T) {
 		fnh,
 		mockNP,
 		time.Second,
+		false,
 		false)
 	eventBroadcaster.StartLogging(klog.Infof)
 
@@ -663,6 +757,123 @@ func TestNodeProvidedIPAddresses(t *testing.T) {
 	updatedNodes := fnh.GetUpdatedNodesCopy()
 	assert.Equal(t, 3, len(updatedNodes[0].Status.Addresses), "Node Addresses not correctly updated")
 	assert.Equal(t, "10.0.0.1", updatedNodes[0].Status.Addresses[0].Address, "Node Addresses not correctly updated")
+}
+
+func Test_reconcileNodeLabels(t *testing.T) {
+	testcases := []struct {
+		name           string
+		labels         map[string]string
+		expectedLabels map[string]string
+		expectedErr    error
+	}{
+		{
+			name:           "no labels",
+			labels:         map[string]string{},
+			expectedLabels: map[string]string{},
+			expectedErr:    nil,
+		},
+		{
+			name: "requires reconcile",
+			labels: map[string]string{
+				v1.LabelZoneFailureDomainStable: "foo",
+				v1.LabelZoneRegionStable:        "bar",
+				v1.LabelInstanceTypeStable:      "the-best-type",
+			},
+			expectedLabels: map[string]string{
+				v1.LabelZoneFailureDomain:       "foo",
+				v1.LabelZoneRegion:              "bar",
+				v1.LabelZoneFailureDomainStable: "foo",
+				v1.LabelZoneRegionStable:        "bar",
+				v1.LabelInstanceType:            "the-best-type",
+				v1.LabelInstanceTypeStable:      "the-best-type",
+			},
+			expectedErr: nil,
+		},
+		{
+			name: "doesn't require reconcile",
+			labels: map[string]string{
+				v1.LabelZoneFailureDomain:       "foo",
+				v1.LabelZoneRegion:              "bar",
+				v1.LabelZoneFailureDomainStable: "foo",
+				v1.LabelZoneRegionStable:        "bar",
+				v1.LabelInstanceType:            "the-best-type",
+				v1.LabelInstanceTypeStable:      "the-best-type",
+			},
+			expectedLabels: map[string]string{
+				v1.LabelZoneFailureDomain:       "foo",
+				v1.LabelZoneRegion:              "bar",
+				v1.LabelZoneFailureDomainStable: "foo",
+				v1.LabelZoneRegionStable:        "bar",
+				v1.LabelInstanceType:            "the-best-type",
+				v1.LabelInstanceTypeStable:      "the-best-type",
+			},
+			expectedErr: nil,
+		},
+		{
+			name: "require reconcile -- secondary labels are different from primary",
+			labels: map[string]string{
+				v1.LabelZoneFailureDomainStable: "foo",
+				v1.LabelZoneRegionStable:        "bar",
+				v1.LabelZoneFailureDomain:       "wrongfoo",
+				v1.LabelZoneRegion:              "wrongbar",
+				v1.LabelInstanceTypeStable:      "the-best-type",
+				v1.LabelInstanceType:            "the-wrong-type",
+			},
+			expectedLabels: map[string]string{
+				v1.LabelZoneFailureDomain:       "foo",
+				v1.LabelZoneRegion:              "bar",
+				v1.LabelZoneFailureDomainStable: "foo",
+				v1.LabelZoneRegionStable:        "bar",
+				v1.LabelInstanceType:            "the-best-type",
+				v1.LabelInstanceTypeStable:      "the-best-type",
+			},
+			expectedErr: nil,
+		},
+	}
+
+	for _, test := range testcases {
+		t.Run(test.name, func(t *testing.T) {
+			testNode := &v1.Node{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:   "node01",
+					Labels: test.labels,
+				},
+			}
+
+			clientset := fake.NewSimpleClientset(testNode)
+			factory := informers.NewSharedInformerFactory(clientset, 0)
+
+			cnc := &CloudNodeController{
+				kubeClient:   clientset,
+				nodeInformer: factory.Core().V1().Nodes(),
+				// Test using the beta toplogy labels.
+				labelReconcileInfo: betaToplogyLabels,
+			}
+
+			// activate node informer
+			factory.Core().V1().Nodes().Informer()
+			factory.Start(nil)
+			factory.WaitForCacheSync(nil)
+
+			err := cnc.reconcileNodeLabels(testNode)
+			if !errors.Is(err, test.expectedErr) {
+				t.Logf("actual err: %v", err)
+				t.Logf("expected err: %v", test.expectedErr)
+				t.Errorf("unexpected error")
+			}
+
+			actualNode, err := clientset.CoreV1().Nodes().Get(context.TODO(), "node01", metav1.GetOptions{})
+			if err != nil {
+				t.Fatalf("error getting updated node: %v", err)
+			}
+
+			if !reflect.DeepEqual(actualNode.Labels, test.expectedLabels) {
+				t.Logf("actual node labels: %v", actualNode.Labels)
+				t.Logf("expected node labels: %v", test.expectedLabels)
+				t.Errorf("updated node did not match expected node")
+			}
+		})
+	}
 }
 
 // Tests that node address changes are detected correctly
