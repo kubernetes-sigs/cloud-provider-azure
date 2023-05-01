30221b72e004b1c6d0dc756c9c1557b081e115a7
MartinForReal <fanshangxiang@gmail.com>
2023-05-01 18:11:43 +0000

    chore(deps): bump github.com/Azure/azure-kusto-go from 0.10.2 to 0.12.1
    Bumps [github.com/Azure/azure-kusto-go](https://github.com/Azure/azure-kusto-go) from 0.10.2 to 0.12.1.
    - [Release notes](https://github.com/Azure/azure-kusto-go/releases)
    - [Changelog](https://github.com/Azure/azure-kusto-go/blob/master/CHANGELOG.md)
    - [Commits](https://github.com/Azure/azure-kusto-go/compare/v0.10.2...v0.12.1)
    
    ---
    updated-dependencies:
    - dependency-name: github.com/Azure/azure-kusto-go
      dependency-type: direct:production
      update-type: version-update:semver-minor
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>

--- vendor/github.com/Azure/azure-kusto-go/kusto/ingest/query_client.go
+++ vendor/github.com/Azure/azure-kusto-go/kusto/ingest/query_client.go
@@ -12,7 +12,8 @@ type QueryClient interface {
 	io.Closer
 	Auth() kusto.Authorization
 	Endpoint() string
-	Query(ctx context.Context, db string, query kusto.Stmt, options ...kusto.QueryOption) (*kusto.RowIterator, error)
-	Mgmt(ctx context.Context, db string, query kusto.Stmt, options ...kusto.MgmtOption) (*kusto.RowIterator, error)
+	Query(ctx context.Context, db string, query kusto.Statement, options ...kusto.QueryOption) (*kusto.RowIterator, error)
+	Mgmt(ctx context.Context, db string, query kusto.Statement, options ...kusto.MgmtOption) (*kusto.RowIterator, error)
 	HttpClient() *http.Client
+	ClientDetails() *kusto.ClientDetails
 }
