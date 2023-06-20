2cef3bb93727f26b61413d31b04cca5458662267
estevan <50840058+chaireze@users.noreply.github.com>
2023-06-20 17:48:39 -0400

    {azure,azure_config}.go: make config checks portable

--- pkg/provider/azure.go
+++ pkg/provider/azure.go
@@ -985,6 +985,26 @@ func (az *Cloud) getAzureClientConfig(servicePrincipalToken *adal.ServicePrincip
 	return azClientConfig
 }
 
+func configChecks(config Config) *Config {
+	// The resource group name may be in different cases from different Azure APIs, hence it is converted to lower here.
+	// See more context at https://github.com/kubernetes/kubernetes/issues/71994.
+	config.ResourceGroup = strings.ToLower(config.ResourceGroup)
+
+	// these environment variables are injected by workload identity webhook
+	if tenantID := os.Getenv("AZURE_TENANT_ID"); tenantID != "" {
+		config.TenantID = tenantID
+	}
+	if clientID := os.Getenv("AZURE_CLIENT_ID"); clientID != "" {
+		config.AADClientID = clientID
+	}
+	if federatedTokenFile := os.Getenv("AZURE_FEDERATED_TOKEN_FILE"); federatedTokenFile != "" {
+		config.AADFederatedTokenFile = federatedTokenFile
+		config.UseFederatedWorkloadIdentityExtension = true
+	}
+
+	return &config
+}
+
 // ParseConfig returns a parsed configuration for an Azure cloudprovider config file
 func ParseConfig(configReader io.Reader) (*Config, error) {
 	var config Config
@@ -1002,22 +1022,9 @@ func ParseConfig(configReader io.Reader) (*Config, error) {
 		return nil, err
 	}
 
-	// The resource group name may be in different cases from different Azure APIs, hence it is converted to lower here.
-	// See more context at https://github.com/kubernetes/kubernetes/issues/71994.
-	config.ResourceGroup = strings.ToLower(config.ResourceGroup)
+	configChecked := configChecks(config)
 
-	// these environment variables are injected by workload identity webhook
-	if tenantID := os.Getenv("AZURE_TENANT_ID"); tenantID != "" {
-		config.TenantID = tenantID
-	}
-	if clientID := os.Getenv("AZURE_CLIENT_ID"); clientID != "" {
-		config.AADClientID = clientID
-	}
-	if federatedTokenFile := os.Getenv("AZURE_FEDERATED_TOKEN_FILE"); federatedTokenFile != "" {
-		config.AADFederatedTokenFile = federatedTokenFile
-		config.UseFederatedWorkloadIdentityExtension = true
-	}
-	return &config, nil
+	return configChecked, nil
 }
 
 func (az *Cloud) isStackCloud() bool {
