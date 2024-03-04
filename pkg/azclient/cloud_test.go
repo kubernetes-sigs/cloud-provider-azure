c9acfe74a3a7c4b952b6d00570ae4e05e6dcb4e5
Theo Barber-Bany <tbarberb@redhat.com>
2024-03-04 16:58:45 +0000

    Handle endpoint not being rerturned in AzureCloudConfigFromURL
    This change updates AzureCloudConfigFromURL to check that the URL
    (ResourceManagerEndpoint) that is used to build the config has been
    returned. If it has not, it sets it using the URL provided.
    
    This avoids the case where the endpoint is used to build config, but
    does not return it's self - causing the created configuration to fail.

--- pkg/azclient/cloud_test.go
+++ pkg/azclient/cloud_test.go
@@ -84,6 +84,61 @@ var _ = Describe("Cloud", func() {
 				Expect(cloudConfig.Services[cloud.ResourceManager].Audience).To(Equal("https://management.core.windows.net/"))
 			})
 		})
+
+		When("the resourceManager is not returned from a valid url", func() {
+			It("should substitute the url into the response", func() {
+				server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+					w.WriteHeader(http.StatusOK)
+					_, err := w.Write([]byte(`
+[
+    {
+        "portal":"https://portal.azure.com",
+        "authentication":{
+            "loginEndpoint":"https://login.microsoftonline.com/",
+            "audiences":[
+                "https://management.core.windows.net/",
+                "https://management.azure.com/"
+            ],
+            "tenant":"common",
+            "identityProvider":"AAD"
+        }
+,
+        "media":"https://rest.media.azure.net",
+        "graphAudience":"https://graph.windows.net/",
+        "graph":"https://graph.windows.net/",
+        "name":"AzureCloud",
+        "suffixes":{
+            "azureDataLakeStoreFileSystem":"azuredatalakestore.net",
+            "acrLoginServer":"azurecr.io",
+            "sqlServerHostname":"database.windows.net",
+            "azureDataLakeAnalyticsCatalogAndJob":"azuredatalakeanalytics.net",
+            "keyVaultDns":"vault.azure.net",
+            "storage":"core.windows.net",
+            "azureFrontDoorEndpointSuffix":"azurefd.net"
+        }
+,
+        "batch":"https://batch.core.windows.net/",
+        "vmImageAliasDoc":"https://raw.githubusercontent.com/Azure/azure-rest-api-specs/master/arm-compute/quickstart-templates/aliases.json",
+        "activeDirectoryDataLake":"https://datalake.azure.net/",
+        "sqlManagement":"https://management.core.windows.net:8443/",
+        "gallery":"https://gallery.azure.com/"
+    }
+
+]
+					`))
+					Expect(err).ToNot(HaveOccurred())
+				}))
+				defer server.Close()
+
+				cloudConfig, err := azclient.AzureCloudConfigFromURL(server.URL)
+				Expect(err).ToNot(HaveOccurred())
+				Expect(cloudConfig).ToNot(BeNil())
+				Expect(cloudConfig.ActiveDirectoryAuthorityHost).To(Equal("https://login.microsoftonline.com/"))
+				Expect(cloudConfig.Services).NotTo(BeEmpty())
+				Expect(cloudConfig.Services[cloud.ResourceManager].Audience).To(Equal("https://management.core.windows.net/"))
+				Expect(cloudConfig.Services[cloud.ResourceManager].Endpoint).To(Equal(server.URL))
+			})
+		})
 	})
 	Context("AzureCloudFromName", func() {
 		When("cloud name is empty", func() {
