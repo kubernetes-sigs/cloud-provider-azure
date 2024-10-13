c18279b2fb0b5f71aae8021e8ea2d51c33cd231a
Jialun Cai <jialun.cai@pm.me>
2024-10-14 10:59:57 +1100

    Optimize CIDR aggregation to improve performance and reduce memory usage

--- pkg/provider/loadbalancer/fnutil/map.go
+++ pkg/provider/loadbalancer/fnutil/map.go
@@ -16,6 +16,14 @@ limitations under the License.
 
 package fnutil
 
+func Keys[K comparable, V any](m map[K]V) []K {
+	rv := make([]K, 0, len(m))
+	for k := range m {
+		rv = append(rv, k)
+	}
+	return rv
+}
+
 func Values[K comparable, V any](m map[K]V) []V {
 	rv := make([]V, 0, len(m))
 	for _, v := range m {
--- pkg/provider/loadbalancer/iputil/prefix.go
+++ pkg/provider/loadbalancer/iputil/prefix.go
@@ -17,8 +17,10 @@ limitations under the License.
 package iputil
 
 import (
+	"bytes"
 	"fmt"
 	"net/netip"
+	"sort"
 )
 
 // IsPrefixesAllowAll returns true if one of the prefixes allows all addresses.
@@ -61,9 +63,108 @@ func GroupPrefixesByFamily(vs []netip.Prefix) ([]netip.Prefix, []netip.Prefix) {
 	return v4, v6
 }
 
-// AggregatePrefixes aggregates prefixes.
-// Overlapping prefixes are merged.
+// ContainsPrefix checks if prefix p fully contains prefix o.
+// It returns true if o is a subset of p, meaning all addresses in o are also in p.
+// This is true when p overlaps with o and p has fewer or equal number of bits than o.
+func ContainsPrefix(p netip.Prefix, o netip.Prefix) bool {
+	return p.Bits() <= o.Bits() && p.Overlaps(o)
+}
+
+// mergeAdjacentPrefixes attempts to merge two adjacent prefixes into a single prefix.
+// It returns the merged prefix and a boolean indicating success.
+// Note: This function only merges adjacent prefixes, not overlapping ones.
+func mergeAdjacentPrefixes(p1, p2 netip.Prefix) (netip.Prefix, bool) {
+	// Merge neighboring prefixes if possible
+	if p1.Bits() != p2.Bits() || p1.Bits() == 0 {
+		return netip.Prefix{}, false
+	}
+
+	var (
+		bits    = p1.Bits()
+		p1Bytes = p1.Addr().AsSlice()
+		p2Bytes = p2.Addr().AsSlice()
+	)
+	if bitAt(p1Bytes, bits-1) == 0 {
+		setBitAt(p1Bytes, bits-1, 1)
+	} else {
+		setBitAt(p2Bytes, bits-1, 1)
+	}
+	if !bytes.Equal(p1Bytes, p2Bytes) {
+		return netip.Prefix{}, false
+	}
+
+	rv, _ := p1.Addr().Prefix(bits - 1)
+	return rv, true
+}
+
+// aggregatePrefixesForSingleIPFamily merges overlapping or adjacent prefixes into a single prefix.
+// The input prefixes must be the same IP family (IPv4 or IPv6).
+// For example,
+// - [192.168.0.0/32, 192.168.0.1/32] -> [192.168.0.0/31] (adjacent)
+// - [192.168.0.0/24, 192.168.0.1/32] -> [192.168.1.0/24] (overlapping)
+func aggregatePrefixesForSingleIPFamily(prefixes []netip.Prefix) []netip.Prefix {
+	if len(prefixes) <= 1 {
+		return prefixes
+	}
+
+	sort.Slice(prefixes, func(i, j int) bool {
+		addrCmp := prefixes[i].Addr().Compare(prefixes[j].Addr())
+		if addrCmp == 0 {
+			return prefixes[i].Bits() < prefixes[j].Bits()
+		}
+		return addrCmp < 0
+	})
+
+	var rv = []netip.Prefix{prefixes[0]}
+
+	for i := 1; i < len(prefixes); i++ {
+		last, p := rv[len(rv)-1], prefixes[i]
+		if ContainsPrefix(last, p) {
+			// Skip overlapping prefixes
+			continue
+		}
+		rv = append(rv, p)
+
+		// Merge adjacent prefixes if possible
+		for len(rv) >= 2 {
+			// Merge the last two prefixes if they are adjacent
+			p, ok := mergeAdjacentPrefixes(rv[len(rv)-2], rv[len(rv)-1])
+			if !ok {
+				break
+			}
+
+			// Replace the last two prefixes with the merged prefix
+			rv = rv[:len(rv)-2]
+			rv = append(rv, p)
+		}
+	}
+	return rv
+}
+
+// AggregatePrefixes merges overlapping or adjacent prefixes into a single prefix.
+// It combines prefixes that can be represented by a larger, more inclusive prefix.
+//
+// Examples:
+//   - Adjacent:    [192.168.0.0/32, 192.168.0.1/32] -> [192.168.0.0/31]
+//   - Overlapping: [192.168.0.0/24, 192.168.0.1/32] -> [192.168.0.0/24]
 func AggregatePrefixes(prefixes []netip.Prefix) []netip.Prefix {
+	var (
+		v4, v6 = GroupPrefixesByFamily(prefixes)
+	)
+
+	return append(aggregatePrefixesForSingleIPFamily(v4), aggregatePrefixesForSingleIPFamily(v6)...)
+}
+
+// AggregatePrefixesWithPrefixTree merges overlapping or adjacent prefixes into a single prefix.
+//
+// This function uses a prefix tree to aggregate the input prefixes. While it achieves
+// the same result as AggregatePrefixes, it is less efficient. For better performance,
+// use AggregatePrefixes instead.
+//
+// Examples:
+//   - Adjacent:   [192.168.0.0/32, 192.168.0.1/32] -> [192.168.0.0/31]
+//   - Overlapping: [192.168.0.0/24, 192.168.0.1/32] -> [192.168.0.0/24]
+func AggregatePrefixesWithPrefixTree(prefixes []netip.Prefix) []netip.Prefix {
 	var (
 		v4, v6 = GroupPrefixesByFamily(prefixes)
 		v4Tree = newPrefixTreeForIPv4()
