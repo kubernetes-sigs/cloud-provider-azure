e1ebdeb09caa82d5f7639656c61664b383ed246f
Jialun Cai <jialun.cai@pm.me>
2024-10-18 10:18:10 +1100

    Fix imports

--- pkg/util/iputil/prefix_test.go
+++ pkg/util/iputil/prefix_test.go
@@ -24,7 +24,7 @@ import (
 
 	"github.com/stretchr/testify/assert"
 
-	"sigs.k8s.io/cloud-provider-azure/pkg/provider/loadbalancer/fnutil"
+	fnutil "sigs.k8s.io/cloud-provider-azure/pkg/util/collectionutil"
 )
 
 func TestIsPrefixesAllowAll(t *testing.T) {
