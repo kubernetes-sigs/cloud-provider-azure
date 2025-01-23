24ee89705d9623641d0dcfa1fc342dd4e8e4733b
Fan Shang Xiang <shafan@microsoft.com>
2025-01-23 17:14:25 +0800

    update test deps before run e2e test

--- hack/test-ccm-e2e.sh
+++ hack/test-ccm-e2e.sh
@@ -23,5 +23,5 @@ LABEL_FILTER="${LABEL_FILTER:-${DEFAULT_LABEL_FILTER}}"
 FLAKE_ATTEMPTS=${FLAKE_ATTEMPTS:-2}
 
 source "${REPO_ROOT}/hack/ensure-ginkgo-v2.sh"
-
+go mod tidy
 ginkgo -flake-attempts ${FLAKE_ATTEMPTS} -skip "${SKIP_ARGS}" -label-filter "${LABEL_FILTER}" "${REPO_ROOT}"/tests/e2e/ --timeout "${CCM_E2E_TIMEOUT:-210m}"
