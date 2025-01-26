b8202e1415a1a5d2b211b9fd60cd597f690c19c0
Qingchuan Hao <qingchuan.hao@microsoft.com>
2025-01-26 17:40:50 +0000

    test: add vm etag in ensureBackendPoolDeletedFromNode and add UT
    Signed-off-by: Qingchuan Hao <qingchuan.hao@microsoft.com>

--- pkg/provider/azure_vmss.go
+++ pkg/provider/azure_vmss.go
@@ -1327,7 +1327,7 @@ func (ss *ScaleSet) ensureVMSSInPool(ctx context.Context, _ *v1.Service, nodes [
 		klog.V(2).Infof("ensureVMSSInPool begins to update vmss(%s) with new backendPoolID %s", vmssName, backendPoolID)
 		rerr := ss.CreateOrUpdateVMSS(ss.ResourceGroup, vmssName, newVMSS)
 		if rerr != nil {
-			klog.Errorf("ensureVMSSInPool CreateOrUpdateVMSS(%s) with new backendPoolID %s, err: %v", vmssName, backendPoolID, err)
+			klog.Errorf("ensureVMSSInPool CreateOrUpdateVMSS(%s) with new backendPoolID %s, err: %v", vmssName, backendPoolID, rerr)
 			return rerr
 		}
 	}
@@ -1626,6 +1626,7 @@ func (ss *ScaleSet) ensureBackendPoolDeletedFromNode(ctx context.Context, nodeNa
 				NetworkInterfaceConfigurations: networkInterfaceConfigurations,
 			},
 		},
+		Etag: vm.Etag,
 	}
 
 	// Get the node resource group.
