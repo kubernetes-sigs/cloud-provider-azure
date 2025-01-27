2ff6f2dfe3e6283599d2dad9cb84618db8148b10
georgeedward2000 <nechitoaiageorgeedward@gmail.com>
2025-01-27 12:59:34 +0200

    update nsg logic for clb + add/refactor tests

---
+++ internal/testutil/fixture/azure_managedcluster.go
@@ -0,0 +1,27 @@
+package fixture
+
+import (
+	armcontainerservice "github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/containerservice/armcontainerservice/v6"
+	"k8s.io/utils/ptr"
+)
+
+func (f *AzureFixture) ManagedCluster() *AzureManagedClusterFixture {
+	return &AzureManagedClusterFixture{
+		mc: &armcontainerservice.ManagedCluster{
+			Name: ptr.To("mangaedcluster"),
+			Properties: &armcontainerservice.ManagedClusterProperties{
+				NetworkProfile: &armcontainerservice.NetworkProfile{
+					PodCidr: ptr.To("192.1.0.0/16"),
+				},
+			},
+		},
+	}
+}
+
+type AzureManagedClusterFixture struct {
+	mc *armcontainerservice.ManagedCluster
+}
+
+func (f *AzureManagedClusterFixture) Build() *armcontainerservice.ManagedCluster {
+	return f.mc
+}
