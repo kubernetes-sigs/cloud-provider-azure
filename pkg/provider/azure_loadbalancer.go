29e5a6f7a297691e17053becf153fdc9b18f608b
Qi Ni <pomelonicky@gmail.com>
2026-04-08 13:21:51 +1000

    refactor: move FirstPartyUsage IP tag type to pkg/consts

--- pkg/provider/azure_loadbalancer.go
+++ pkg/provider/azure_loadbalancer.go
@@ -3629,12 +3629,10 @@ func (az *Cloud) ensurePIPIPTagged(service *v1.Service, pip *armnetwork.PublicIP
 		return false, nil
 	}
 
-	const firstPartyUsageType = "FirstPartyUsage"
-
 	// Split current PIP IP tags into FirstPartyUsage and other.
 	var currentFPU, currentOther []*armnetwork.IPTag
 	for _, tag := range pip.Properties.IPTags {
-		if strings.EqualFold(ptr.Deref(tag.IPTagType, ""), firstPartyUsageType) {
+		if strings.EqualFold(ptr.Deref(tag.IPTagType, ""), consts.IPTagTypeFirstPartyUsage) {
 			currentFPU = append(currentFPU, tag)
 		} else {
 			currentOther = append(currentOther, tag)
@@ -3644,7 +3642,7 @@ func (az *Cloud) ensurePIPIPTagged(service *v1.Service, pip *armnetwork.PublicIP
 	// Split desired IP tags into FirstPartyUsage and other.
 	var desiredFPU, desiredOther []*armnetwork.IPTag
 	for _, tag := range ipTagRequest.IPTags {
-		if strings.EqualFold(ptr.Deref(tag.IPTagType, ""), firstPartyUsageType) {
+		if strings.EqualFold(ptr.Deref(tag.IPTagType, ""), consts.IPTagTypeFirstPartyUsage) {
 			desiredFPU = append(desiredFPU, tag)
 		} else {
 			desiredOther = append(desiredOther, tag)
