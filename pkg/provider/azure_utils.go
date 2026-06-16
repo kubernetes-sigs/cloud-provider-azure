54df699224f3cc7ba6743b7090ce52e8d0480aa2
Liunardy <54991798+Liunardy@users.noreply.github.com>
2026-06-16 06:41:04 +0000

    fix: getServicePIPPrefixID returns empty for IPv6 on IPv4-only service

--- pkg/provider/azure_utils.go
+++ pkg/provider/azure_utils.go
@@ -418,6 +418,9 @@ func getServicePIPPrefixID(service *v1.Service, isIPv6 bool) string {
 				return id
 			}
 		}
+		if isIPv6 && !v6Enabled {
+			return ""
+		}
 		return service.Annotations[consts.ServiceAnnotationPIPPrefixIDDualStack[false]]
 	}
 
