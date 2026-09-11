9de4f8b439550c3d45429fe0654466c6e7d54799
Qi Ni <pomelonicky@gmail.com>
2026-09-11 16:50:58 +1000

    fix: use stable gRPC v1.83.2 for CVE remediation

--- vendor/google.golang.org/grpc/internal/buffer/unbounded.go
+++ vendor/google.golang.org/grpc/internal/buffer/unbounded.go
@@ -27,29 +27,32 @@ import (
 // extra goroutines. This is typically used for passing updates from one entity
 // to another within gRPC.
 //
-// To avoid extra memory allocations and type assertions, using any on
-// performance-critical code paths is discouraged. Use concrete types wherever
-// possible when instantiating Unbounded.
-//
 // All methods on this type are thread-safe and don't block on anything except
 // the underlying mutex used for synchronization.
-type Unbounded[T any] struct {
-	c       chan T
+//
+// Unbounded supports values of any type to be stored in it by using a channel
+// of `any`. This means that a call to Put() incurs an extra memory allocation,
+// and also that users need a type assertion while reading. For performance
+// critical code paths, using Unbounded is strongly discouraged and defining a
+// new type specific implementation of this buffer is preferred. See
+// internal/transport/transport.go for an example of this.
+type Unbounded struct {
+	c       chan any
 	closed  bool
 	closing bool
 	mu      sync.Mutex
-	backlog []T
+	backlog []any
 }
 
 // NewUnbounded returns a new instance of Unbounded.
-func NewUnbounded[T any]() *Unbounded[T] {
-	return &Unbounded[T]{c: make(chan T, 1)}
+func NewUnbounded() *Unbounded {
+	return &Unbounded{c: make(chan any, 1)}
 }
 
 var errBufferClosed = errors.New("Put called on closed buffer.Unbounded")
 
 // Put adds t to the unbounded buffer.
-func (b *Unbounded[T]) Put(t T) error {
+func (b *Unbounded) Put(t any) error {
 	b.mu.Lock()
 	defer b.mu.Unlock()
 	if b.closing {
@@ -69,14 +72,13 @@ func (b *Unbounded[T]) Put(t T) error {
 // Load sends the earliest buffered data, if any, onto the read channel returned
 // by Get(). Users are expected to call this every time they successfully read a
 // value from the read channel.
-func (b *Unbounded[T]) Load() {
+func (b *Unbounded) Load() {
 	b.mu.Lock()
 	defer b.mu.Unlock()
 	if len(b.backlog) > 0 {
 		select {
 		case b.c <- b.backlog[0]:
-			var zero T
-			b.backlog[0] = zero
+			b.backlog[0] = nil
 			b.backlog = b.backlog[1:]
 		default:
 		}
@@ -94,14 +96,14 @@ func (b *Unbounded[T]) Load() {
 //
 // If the unbounded buffer is closed, the read channel returned by this method
 // is closed after all data is drained.
-func (b *Unbounded[T]) Get() <-chan T {
+func (b *Unbounded) Get() <-chan any {
 	return b.c
 }
 
 // Close closes the unbounded buffer. No subsequent data may be Put(), and the
 // channel returned from Get() will be closed after all the data is read and
 // Load() is called for the final time.
-func (b *Unbounded[T]) Close() {
+func (b *Unbounded) Close() {
 	b.mu.Lock()
 	defer b.mu.Unlock()
 	if b.closing {
